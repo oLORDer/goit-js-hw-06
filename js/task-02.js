30792792c927f22eca0093e9199d51f914af4054
oLORDer <justsendmeow@gmail.com>
2022-06-28 20:31:45 +0200

    2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.getElementById('ingredients');
+
+function addNewItem(element, arr) {
+  arr.forEach((item) => {
+    let li = document.createElement('li');
+    li.classList.add('item');
+    li.textContent = item;
+    element.append(li);
+  });
+}
+
+addNewItem(ingredientsList, ingredients);
