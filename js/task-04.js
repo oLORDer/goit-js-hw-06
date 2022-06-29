3150042f12df0cdc02ee4c4beb355ff51aa162fa
Andrii <andryushka137@gmail.com>
2022-06-29 14:07:17 +0300

    4

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,12 @@
-function got() {}
+const btnDecrement = document.querySelector('button[data-action="decrement"]'),
+  btnIncrement = document.querySelector('button[data-action="increment"]'),
+  counterValue = document.querySelector('#value');
+counterValue.textContent = 0;
+
+btnIncrement.addEventListener('click', () => {
+  counterValue.textContent++;
+});
+
+btnDecrement.addEventListener('click', () => {
+  counterValue.textContent--;
+});
