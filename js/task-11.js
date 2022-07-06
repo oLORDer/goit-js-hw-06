737d3161a0afdd6cfd14e3ea02bc82221ce42f46
oLORDer <justsendmeow@gmail.com>
2022-07-06 19:35:27 +0300

    fixed 11

--- js/task-11.js
+++ js/task-11.js
@@ -8,24 +8,36 @@ const cities = [
 ];
 const body = document.querySelector('body');
 
-function addNewItem(element, arr) {
-  arr.forEach((item) => {
-    let li = document.createElement('li');
-    element.append(li);
-
-    if (typeof item === 'object') {
-      let ul = document.createElement('ul');
-      li.append(ul);
-      for (let i = 0; i < item.length; i++) {
-        let secondli = document.createElement('li');
-        ul.append(secondli);
-        secondli.textContent = item[i];
-      }
-    } else {
-      li.textContent = item;
+const renderItems = (arr) => {
+  const list = arr.map((el) => {
+    if (Array.isArray(el)) {
+      return renderItems(el);
     }
+    return `<li>${el}</li>`;
   });
-}
-let list = document.createElement('ul');
-body.append(list);
-addNewItem(list, cities);
+  return `<ul>${list.join('')}</ul>`;
+};
+renderItems(cities);
+document.body.innerHTML = renderItems(cities);
+
+// function addNewItem(element, arr) {
+//   arr.forEach((item) => {
+//     let li = document.createElement('li');
+//     element.append(li);
+
+//     if (typeof item === 'object') {
+//       let ul = document.createElement('ul');
+//       li.append(ul);
+//       for (let i = 0; i < item.length; i++) {
+//         let secondli = document.createElement('li');
+//         ul.append(secondli);
+//         secondli.textContent = item[i];
+//       }
+//     } else {
+//       li.textContent = item;
+//     }
+//   });
+// }
+// let list = document.createElement('ul');
+// body.append(list);
+// addNewItem(list, cities);
